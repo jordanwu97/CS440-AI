fa94bb5854e501c00f22af1478534c697c34563d
Jordan Wu <jordan@Jordans-MacBook-Pro.local>
2019-04-10 22:57:12 -0500

    yeh idk whats going wrong with net

--- mp4/part 2/neural_network.py
+++ mp4/part 2/neural_network.py
@@ -22,9 +22,22 @@ import numpy as np
         (Stdout print will not affect autograder as long as runtime is within limits)
 """
 def minibatch_gd(epoch, w1, w2, w3, w4, b1, b2, b3, b4, x_train, y_train, num_classes, shuffle=True):
+ 
+    losses = []
 
-    #IMPLEMENT HERE
-    Z1, cache = affine_forward(x_train, w1, b1)
+    W = np.array([w1,w2,w3,w4], dtype=object)
+    B = np.array([b1,b2,b3,b4], dtype=object)
+
+    for ep in range(epoch):
+
+        randargs = np.random.choice(len(x_train), size=1000, replace=False)
+        x_batch = x_train[randargs]
+        y_batch = y_train[randargs]
+
+        loss = four_nn(W,B,x_batch,y_batch, num_classes, test=False)
+        losses.append(loss)
+
+    print (losses)
 
     return w1, w2, w3, w4, b1, b2, b3, b4, losses
 
@@ -46,7 +59,14 @@ def minibatch_gd(epoch, w1, w2, w3, w4, b1, b2, b3, b4, x_train, y_train, num_cl
 """
 def test_nn(w1, w2, w3, w4, b1, b2, b3, b4, x_test, y_test, num_classes):
 
-    avg_class_rate = 0.0
+    W = np.array([w1,w2,w3,w4], dtype=object)
+    B = np.array([b1,b2,b3,b4], dtype=object)
+
+    classification = four_nn(W,B,x_test,y_test, num_classes, test=True)
+
+    print (classification)
+
+    avg_class_rate = np.sum(np.equal(classification, y_test)) / len(x_test)
     class_rate_per_class = [0.0] * num_classes
     return avg_class_rate, class_rate_per_class
 
@@ -56,8 +76,42 @@ def test_nn(w1, w2, w3, w4, b1, b2, b3, b4, x_test, y_test, num_classes):
     Up to you on how to implement this, won't be unit tested
     Should call helper functions below
 """
-def four_nn():
-    pass
+def four_nn(W, B, x, y, num_classes, test=False):
+
+    def forward_prop(a,w,b):
+        z, affineCache = affine_forward(a,w,b)
+        a_1, activationCache = relu_forward(z)
+        return a_1, affineCache, activationCache
+
+    def back_prop(dA_1, activationCache, affineCache):
+        dZ = relu_backward(dA_1, activationCache)
+        dA, dW, dB = affine_backward(dZ, affineCache)
+        return dA, dW, dB
+
+    dW = np.array([ np.ones(w.shape) for w in W ], dtype=object)
+    dB = np.array([ np.ones(b.shape) for b in B ], dtype=object)
+    
+    a1, affineCache1, activationCache1 = forward_prop(x, W[0], B[0])
+    a2, affineCache2, activationCache2 = forward_prop(a1, W[1], B[1])
+    a3, affineCache3, activationCache3 = forward_prop(a2, W[2], B[2])
+    F, affineCache4 = affine_forward(a3,W[3],B[3])
+
+    if test == True:
+        classification = np.argmax(F, axis=1)
+        return classification
+
+    # back prop
+    loss, dF = cross_entropy(F, y)
+    dA4, dW[3], dB[3] = affine_backward(dF, affineCache4)
+    dA3, dW[2], dB[2] = back_prop(dA4, activationCache3, affineCache3)
+    dA2, dW[1], dB[1] = back_prop(dA3, activationCache2, affineCache2)
+    dA1, dW[0], dB[0] = back_prop(dA2, activationCache1, affineCache1)
+
+    # update weights
+    eta = 0.1
+    W -= eta * dW
+    B -= eta * dB
+    return loss
 
 """
     Next five functions will be used in four_nn() as helper functions.
@@ -97,6 +151,7 @@ def cross_entropy(F, y):
     # calculate Softmax
     exp_ij = np.exp(F)
     exp_i = np.sum(exp_ij, axis=1)
+    softMax = (exp_ij / exp_i[:,None])
 
     # calculate loss
     F_iyi = F[np.arange(n), y.astype(int)]
@@ -105,6 +160,6 @@ def cross_entropy(F, y):
     # calculate dF
     one_hot = np.zeros(F.shape)
     one_hot[np.arange(n),y.astype(int)] = 1
-    dF = (one_hot - (exp_ij / exp_i[:,None])) / n * -1
+    dF = (one_hot - softMax) / n * -1
 
     return loss, dF
